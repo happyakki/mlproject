a47220ecd2c233669de273e0dede4feb69a99421
happyakki <greenstepankit.com>
2024-12-06 10:20:21 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,10 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -17,19 +19,30 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
 
     try:
         report = {}
 
         for i in range(len(models)):
+            
             model =list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
+            
+            #model.fit(x_train,y_train)
 
             y_train_pred =model.predict(x_train)
+
             y_test_pred = model.predict(x_test)
 
             train_model_score = r2_score(y_train, y_train_pred)    
+
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
@@ -38,4 +51,11 @@ def evaluate_models(x_train,y_train,x_test,y_test,models):
     
     except Exception as e:
         raise ConnectionError(e,sys)
-    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
